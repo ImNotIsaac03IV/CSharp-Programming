632295768fe1aee06a64c3108f1a50d939fcf07f
ImNotIsaac03IV <isaac.darko04@outlook.com>
2024-12-16 08:59:35 +0100

    Add TextCompress

---
+++ TextCompress/Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TextCompress
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a sentence to compress: ");
+            string input = Console.ReadLine();
+            int start = 0;
+            int end = input.Length - 1;
+            string output = "";
+
+            while (start <= input.Length && input[start] == ' ')
+            {
+                start++;
+            }
+            while (end >= input.Length && input[end] == ' ')
+            {
+                end--;
+            }
+            bool previousWasSpace = false;
+            for (int i = start; i <= end; i++)
+            {
+                char c = input[i];
+                if (c == ' ')
+                {
+                    if (!previousWasSpace)
+                    {
+                        output += input[i];
+                        previousWasSpace = true;
+                    }
+                }
+                else
+                {
+                    output += c;
+                    previousWasSpace = false;
+                }
+            }
+            Console.WriteLine($"Compressed form: {output}");
+            Console.ReadKey();
+        }
+    }
+}
