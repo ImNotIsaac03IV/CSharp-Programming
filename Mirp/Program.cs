0b0350ef0ff78f1c4ded12649d6f5822f344b421
ImNotIsaac03IV <isaac.darko04@outlook.com>
2025-01-15 15:03:23 +0100

    Rewrote whole code

--- Mirp/Program.cs
+++ Mirp/Program.cs
@@ -10,74 +10,61 @@ namespace Mirp
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter a number: ");
-            isMirp();
-            Console.ReadKey();
-        }
-        static void isMirp()
-        {
-            bool reversedInputIsPrime = false;
-            string reversedInput = "";
-            bool inputIsPrime = false;
+
             int counter = 0;
-            int number;
-            string input = Console.ReadLine();
 
-            while (!int.TryParse(input, out number) || !(number > 10 && number < 1000))
+            for (int i = 10; i < 1000; i++)
             {
-                Console.WriteLine("input must be a number between 10 and 1000");
-                Console.Write("Enter a number: ");
-                input = Console.ReadLine();
+                if (IsMirp(i))
+                {
+                    Console.Write($"{i}, ");
+                    counter++;
+
+                    if (counter % 10 == 0)
+                    {
+                        Console.WriteLine();
+                    }
+                }
             }
+            Console.WriteLine();
+        }
 
-            for (int i = 1; i <= number; i++)
+        static bool isPrim(int numberInput)
+        {
+            int counter = 0;
+            for (int i = 1; i <= numberInput; i++)
             {
-                if (number % i == 0)
+                if (numberInput % i == 0)
                 {
                     counter++;
                 }
             }
             if (counter == 2)
             {
-                inputIsPrime = true;
+                return true;
             }
-            else
-            {
-                inputIsPrime = false;
-            }
-            counter = 0;
+            return false;
+        }
 
-            while (number > 0)
+        static int reversedNumberInput(int numberInput)
+        {
+            string reversedNumberInput = "";
+            while (numberInput > 0)
             {
-                int remainder = number % 10;
-                reversedInput = reversedInput + remainder.ToString();
-                number /= 10;
+                int remainder = numberInput % 10;
+                reversedNumberInput = reversedNumberInput + remainder.ToString();
+                numberInput /= 10;
             }
+            return int.Parse(reversedNumberInput);
+        }
 
-            for (int i = 1; i <= 1000; i++)
+        static bool IsMirp(int numberInput)
+        {
+            if (isPrim(numberInput) && isPrim(reversedNumberInput(numberInput)))
             {
-                if (int.Parse(reversedInput) % i == 0)
-                {
-                    counter++;
-                } 
+                return true;
             }
-            if (counter == 2)
-            {
-                reversedInputIsPrime = true;
-            }
-            else
-            {
-                reversedInputIsPrime = false;
-            }
-
-            if (reversedInputIsPrime && inputIsPrime)
-            {
-                Console.WriteLine("Your input is a mirp number!!!");
-            }
-            else
-            {
-                Console.WriteLine("Your input isn't a mirp number...");
-            }
+            return false;
         }
     }
 }
