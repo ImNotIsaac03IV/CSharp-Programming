eae3972d440b14cb002576978dc0f6ead2e3b831
ImNotIsaac03IV <isaac.darko04@outlook.com>
2025-03-01 23:17:32 +0100

    add SVNumber

---
+++ SV-Nummer/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SV_Nummer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Check a SV Number");
+            Console.WriteLine("*************************");
+
+            string input;
+            int[] weight = { 3, 7, 9, 0, 5, 8, 4, 2, 1, 6 };
+            Console.Write("Please enter a SV Number: ");
+            input = Console.ReadLine();
+
+            if (CheckDigit(input, weight))
+            {
+                Console.WriteLine($"The SV Number \"{input}\" is valid");
+            }
+            else
+            {
+                Console.WriteLine($"The SV Number \"{input}\" is invalid");
+            }
+        }
+        static int LengthSVNumber(string input)
+        {
+            return input.Length;
+        }
+        static bool ValidateSVNumber(string input)
+        {
+            while(!int.TryParse(input, out _))
+            {
+                return false;
+            }
+            return true;
+        }
+        static bool CheckDigit(string input, int[] weight)
+        {
+            int sum = 0;
+            int rest;
+            for (int i = 0; i < input.Length; i++)
+            {
+                int digit = input[i] - '0';
+                sum += (digit * weight[i]);
+            }
+            rest = sum % 11;
+            return rest == input[3] - '0';
+        }
+        static bool CheckSVNumber(string input, int[] weight)
+        {
+            if (LengthSVNumber(input) != 10)
+            {
+                return false;
+            }
+            if (!ValidateSVNumber(input))
+            {
+                return false;
+            }
+            if (!CheckDigit(input, weight))
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
