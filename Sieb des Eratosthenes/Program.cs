37bd6bf6f82f21ee7a6c29b4a19580c24d3df2e5
ImNotIsaac03IV <isaac.darko04@outlook.com>
2025-03-05 06:43:09 +0100

    Add Sieb des Eratosthenes

---
+++ Sieb des Eratosthenes/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sieb_des_Eratosthenes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = UserInput("Bis zu welcher Zahl wollen Sie Primzahlen ausgeben? ");
+            Console.WriteLine($"Primzahlen von 1 - {input}");
+            for (int i = 2; i < input; i++)
+            {
+                if (IsPrime(i))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+        static int UserInput(string message)
+        {
+            Console.Write(message);
+            int input;
+            while (!int.TryParse(Console.ReadLine(), out input) || input < 2)
+            {
+                Console.Write("Ungültiger Eingabe. Eingabe wiederholen: ");
+            }
+            return input;
+        }
+        static bool IsPrime(int input)
+        {
+            if (input == 1)
+            {
+                return false;
+            }
+
+            for (int i = 2; i <= Math.Sqrt(input); i++)
+            {
+                if (input % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
