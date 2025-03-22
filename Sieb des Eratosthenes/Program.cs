d6911d13f6439bd7ca9cd4ec6cd2fff736864b55
ImNotIsaac03IV <isaac.darko04@outlook.com>
2025-03-22 13:59:57 +0100

    rewrite whole code

--- Sieb des Eratosthenes/Program.cs
+++ Sieb des Eratosthenes/Program.cs
@@ -8,44 +8,62 @@ namespace Sieb_des_Eratosthenes
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            int input = UserInput("Bis zu welcher Zahl wollen Sie Primzahlen ausgeben? ");
-            Console.WriteLine($"Primzahlen von 1 - {input}");
-            for (int i = 2; i < input; i++)
+            Console.Write("Bis zu welcher Zahl wollen Sie Primzahlen ausgeben? ");
+            int input = ReadUserInput();
+
+            bool[] isPrime = SiebDesEra(input);
+
+            int counter = 0;
+            for (int i = 0; i < input; i++)
             {
-                if (IsPrime(i))
+                if (isPrime[i])
                 {
                     Console.WriteLine(i);
+                    counter++;
+
+                    if (counter == 22)
+                    {
+                        Console.WriteLine("<Eingabetaste für ENDE>");
+                        Console.ReadLine();
+                        counter = 0;
+                    }
                 }
             }
         }
-        static int UserInput(string message)
+        static int ReadUserInput()
         {
-            Console.Write(message);
             int input;
-            while (!int.TryParse(Console.ReadLine(), out input) || input < 2)
+            while (!int.TryParse(Console.ReadLine(), out input) || input <= 1)
             {
-                Console.Write("Ungültiger Eingabe. Eingabe wiederholen: ");
+                Console.Write("Input must be greater than 1: ");
             }
             return input;
         }
-        static bool IsPrime(int input)
+        static bool[] SiebDesEra(int size)
         {
-            if (input == 1)
+            bool[] isPrime = new bool[size];
+            isPrime[0] = false;
+            isPrime[1] = false;
+
+            for (int i = 2; i < size; i++)
             {
-                return false;
+                isPrime[i] = true;
             }
 
-            for (int i = 2; i <= Math.Sqrt(input); i++)
+            for (int i = 2; i * i < size; i++)
             {
-                if (input % i == 0)
+                if (isPrime[i])
                 {
-                    return false;
+                    for (int j = i * i; j < size; j += i)
+                    {
+                        isPrime[j] = false;
+                    }
                 }
             }
 
-            return true;
+            return isPrime;
         }
     }
 }
